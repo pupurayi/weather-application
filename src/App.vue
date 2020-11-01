b1a6ae3cf8dc7cf7b88ca433775203ec6b53a2b6
Pupurayi <paulapmarisa463@gmail.com>
2020-11-01 15:05:16 +0200

    Update App.vue
    the background image

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
-  <div id="app">
+  <div id="app" :class="typeof weather.main != 'undefined' && weather.main.temp
+  > 16 ? 'warm': ''">
     <main>
       <div class="search-box">
         <input
@@ -83,7 +84,10 @@ body{
   background-size: cover;
   background-position: bottom;
   transition: 0.4s;
+}
 
+#app .warm{
+  background-image:url(./assets/warm-bg.jpg) ;
 }
 main{
   min-height: 100vh;
